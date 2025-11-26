68902b42147a60d02ea9ea2355d7e49b7f2d754a
martincive <martincivetta@gmail.com>
2025-11-26 18:13:24 -0300

    back terminado

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from rag_pipeline import build_qa
 from sse_starlette.sse import EventSourceResponse
+import asyncio
 
 app = FastAPI()
 qa = build_qa()
@@ -17,7 +18,25 @@ def health():
 async def chat_stream(q: Question):
 
     async def event_generator():
-        for chunk in qa.stream(q.question):
-            yield {"data": chunk.content}
+
+        async for chunk in qa.astream(q.question):
+
+            text = chunk.content
+
+            if not text:
+                continue
+
+            yield {
+                "event": "message",
+                "data": text,
+            }
+
+            await asyncio.sleep(0)
+
+        yield {
+            "event": "done",
+            "data": "end"
+        }
+
 
     return EventSourceResponse(event_generator())
